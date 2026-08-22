c430c346a0657f0ff9d200a25dfa282276e5a853
璐 <lu@lus-Mac-Studio.local>
2026-08-22 16:09:56 +0800

    Release synchronized quota client 1.2.30

--- Sources/CodexTrafficLightCore/ClientUpdate.swift
+++ Sources/CodexTrafficLightCore/ClientUpdate.swift
@@ -2,7 +2,7 @@ import CryptoKit
 import Foundation
 
 public enum ClientVersion {
-    public static let current = "1.2.29"
+    public static let current = "1.2.30"
     public static let signingPublicKeyBase64 = "kx2EJhi8RR4A+CTuoSs4Fx5f59+oicxN5z9wMPra3nc="
 
     public static func compare(_ left: String, _ right: String) -> ComparisonResult {
